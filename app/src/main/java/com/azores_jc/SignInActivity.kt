77194841e37ff0039c12f97e6783bed25b097f36
ralphrenson2004 <ralphrensonenisimo31@gmail.com>
2026-04-07 09:41:45 +0800

    Update SignInActivity.kt

--- app/src/main/java/com/azores_jc/SignInActivity.kt
+++ app/src/main/java/com/azores_jc/SignInActivity.kt
@@ -24,7 +24,7 @@ class SignInActivity : AppCompatActivity() {
     // Hardcoded account credentials
     companion object {
         private const val HARDCODED_USERNAME = "admin"
-        private const val HARDCODED_PASSWORD = ""
+        private const val HARDCODED_PASSWORD = "password123"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
